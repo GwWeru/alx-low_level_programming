7e56a8ad41ee6ce786bf6773632d8421391794b7
weru7 <georgeweru143@gmail.com>
2022-07-01 11:24:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,23 +3,16 @@
 
 
 /**
- *
- * * main - Prints "Programming is like building a multilingual
- *
- * *                puzzle, followed by a new line."
- *
- * *
- *
- * * Return: Always 0.
- *
- * */
+ * main - Prints "Programming is like building a multilingual*
+ *                puzzle, followed by a new line."*
+ * Return: Always 0.
+ */
 
 int main(void)
 
 {
 
 		puts("\"Programming is like building a multilingual puzzle");
-
 
 
 			return (0);
