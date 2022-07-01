f4c8bb1528576bf51b2c658d9406290e96832a9b
weru7 <georgeweru143@gmail.com>
2022-07-01 07:04:45 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
 
+#include <string.h>
+
 
 
 /**
  *
- *  * main - Entry point
+ * *main - Main function of my project
  *
- *   *
+ * *
  *
- *    * Return: Always 0 (Success)
+ * *
  *
- *     */
+ * *Return: return 0 everytime
+ *
+ * */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+
+		char displayed[52];
+
+
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
 
 }
