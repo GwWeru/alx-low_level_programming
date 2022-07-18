65c080920e22b2c60d9c1ed9f63153ce957585a1
weru7 <georgeweru143@gmail.com>
2022-07-18 11:46:40 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			(needle[j] != haystack[i + j])
+			if (needle[j] != haystack[i + j])
 			{
 				break;
 			}
