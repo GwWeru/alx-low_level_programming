0497099f95c0d54a0c77e329b5c0f4e57d104466
weru7 <georgeweru143@gmail.com>
2022-07-26 13:54:17 -0700

    str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,59 @@
 #include "main.h"
 
+#include <stdio.h>
+
 #include <stdlib.h>
 
+
+
 /**
- * str_concat -> string concatinating function
- * @s1: string 1
- * @s2: string 2
- * Return: string 1 + string 2
+ * alloc_grid - Entry point
+ * @width: rows of matrix
+ * @height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
 
-char *str_concat(char *s1, char *s2)
-{
-	int i = 0, j = 0, k = 0,  l = 0;
-	char *s;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s1 = "";
-	while (s1[i])
-		i++;
-	while (s2[j])
-		j++;
-	l = i + j;
-	s = (char *)malloc(l * sizeof(char) + 1);
-	if (s == NULL)
+
+int **alloc_grid(int width, int height)
+
+{
+
+	int **matrix;
+
+	int i;
+
+	int j;
+
+	int l;
+
+	int *p;
+
+	if (width <= 0 || height <= 0)
 		return (NULL);
-	j = 0;
-	while (k < l)
+	matrix = (int **)malloc(height * sizeof(int *));
+	if (matrix == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
-		if (k < i)
-			s[k] = s1[k];
-		if (k >= l)
+		*(matrix + i) = (int *)malloc(width * sizeof(int));
+		if (*(matrix + i) == NULL)
 		{
-			s[k] = s2[j];
-			j++;
+			for (i = 0; i < height; i++)
+		{
+			p = matrix[i];
+				free(p);
+			}
+			free(matrix);
+			return (NULL);
 		}
-		k++;
 	}
-	s[k] = '\0';
-	return  (s);
-}
+	for (l = 0; l < height; l++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matrix[l][j] = 0;
+		}
+	}
+	return (matrix);
+
