2a6279c68881bbc232ba41de363231aaab91d5cb
weru7 <georgeweru143@gmail.com>
2022-12-07 12:59:46 -0800

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,8 @@
 function island_perimeter
 returns the perimeter of the island described in grid
 """
+
+
 def island_perimeter(grid):
     """ returns the perimeter of the island """
     aux = 0
@@ -15,4 +17,4 @@ def island_perimeter(grid):
                     aux -= 2
                 if y > 0 and grid[y - 1][x]:
                     aux -= 2
-return aux
+    return aux
